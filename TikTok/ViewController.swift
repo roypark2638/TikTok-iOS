d055b8412fe8428dfe1f799b801551a8bc86b213
Roy Park <roypark2638@gmail.com>
2021-03-18 01:53:22 -0700

    Setting up project

---
+++ TikTok/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TikTok
+//
+//  Created by Roy Park on 3/18/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+
+}
+
