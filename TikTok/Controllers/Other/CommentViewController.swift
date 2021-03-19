8bde5a9636a82b33aa57246e0972dd851be430db
Roy Park <roypark2638@gmail.com>
2021-03-19 04:26:46 -0700

    Implemented the basic UI and features of the CommentVC

--- TikTok/Controllers/Other/CommentViewController.swift
+++ TikTok/Controllers/Other/CommentViewController.swift
@@ -7,9 +7,35 @@
 
 import UIKit
 
+protocol CommentViewControllerDelegate: AnyObject {
+    func didTapCloseForComments(with viewController: CommentViewController)
+}
+
 class CommentViewController: UIViewController {
     
-    init(model: PostModel) {
+    private let tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.register(
+            UITableViewCell.self,
+            forCellReuseIdentifier: "cell"
+        )
+        return tableView
+    }()
+    
+    weak var delegate: CommentViewControllerDelegate?
+    
+    private var comments = [PostComment]()
+    
+    private let closeButton: UIButton = {
+        let button = UIButton()
+        button.setBackgroundImage(UIImage(systemName: "xmark"), for: .normal)
+        button.layoutIfNeeded()
+        button.contentMode = .scaleAspectFit
+        button.tintColor = .black
+        return button
+    }()
+    
+    init(post: PostModel) {
         super.init(nibName: nil, bundle: nil)
     }
     
@@ -19,9 +45,43 @@ class CommentViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        view.backgroundColor = .white
+        fetchPostComments()
+        view.addSubview(closeButton)
+        view.addSubview(tableView)
+        closeButton.addTarget(self, action: #selector(didTapClose), for: .touchUpInside)
+        tableView.delegate = self
+        tableView.dataSource = self
     }
     
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        closeButton.frame = CGRect(x: view.width - 45, y: 10, width: 35, height: 35)
+        tableView.frame = CGRect(x: 0, y: closeButton.bottom, width: view.width, height: view.height - closeButton.bottom)
+    }
+    
+    private func fetchPostComments() {
+        self.comments = PostComment.mockComments()
+        
+    }
+    
+    @objc private func didTapClose() {
+        delegate?.didTapCloseForComments(with: self)
+    }
 
+}
 
+extension CommentViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return comments.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        let comment = comments[indexPath.row]
+        cell.textLabel?.text = comment.text
+//        cell.textLabel?.text = "first comment for this post!"
+        
+        return cell
+    }
 }
