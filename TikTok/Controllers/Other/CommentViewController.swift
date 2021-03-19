2738d345ab140b5c09fc16b1ccaeb0d46b29901e
Roy Park <roypark2638@gmail.com>
2021-03-19 01:43:56 -0700

    CommentVC basic setup

---
+++ TikTok/Controllers/Other/CommentViewController.swift
@@ -0,0 +1,27 @@
+//
+//  CommentViewController.swift
+//  TikTok
+//
+//  Created by Roy Park on 3/18/21.
+//
+
+import UIKit
+
+class CommentViewController: UIViewController {
+    
+    init(model: PostModel) {
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError()
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+
+}
