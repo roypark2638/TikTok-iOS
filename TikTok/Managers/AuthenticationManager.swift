857a23a7ea2931ad1130a2e03b13d38f9f530c70
Roy Park <roypark2638@gmail.com>
2021-03-19 23:36:56 -0700

    AuthManager set up

--- TikTok/Managers/AuthenticationManager.swift
+++ TikTok/Managers/AuthenticationManager.swift
@@ -6,3 +6,41 @@
 //
 
 import Foundation
+import FirebaseAuth
+
+struct AuthManager {
+    public static let shared = AuthManager()
+    
+    private init() {}
+    
+    enum SignInMethod {
+        case email
+        case Facebook
+        case Google
+    }
+    
+    // Public
+    
+    public var isSignedIn: Bool {
+        return Auth.auth().currentUser != nil
+    }
+    
+    public func SignIn(with method: SignInMethod) {
+        
+    }
+    
+    
+    /// Signing out the current user
+    /// - Parameter completion: async callback to return true or false based on the result
+    public func signOut(completion: (Bool) -> Void) {
+        do {
+            try Auth.auth().signOut()
+            completion(true)
+        }
+        catch {
+            print(error)
+            completion(false)
+        }
+        
+    }
+}
