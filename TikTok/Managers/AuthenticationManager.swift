d9093f48ea689285bf562da83f270a864da90902
Roy Park <roypark2638@gmail.com>
2021-03-21 01:31:48 -0700

    Implemented Firebase Email Auth and Database

--- TikTok/Managers/AuthenticationManager.swift
+++ TikTok/Managers/AuthenticationManager.swift
@@ -15,8 +15,13 @@ struct AuthManager {
     
     enum SignInMethod {
         case email
-        case Facebook
-        case Google
+//        case Facebook
+//        case Google
+    }
+    
+    enum AuthError: Error {
+        case SignInFailed
+        case SignUpFailed
     }
     
     // Public
@@ -25,8 +30,55 @@ struct AuthManager {
         return Auth.auth().currentUser != nil
     }
     
-    public func SignIn(with method: SignInMethod) {
+    public func SignIn(with method: SignInMethod, email: String?, password: String?, completion: @escaping (Result<String, Error>) -> Void) {
+        switch method {
+        case .email:
+            guard let email = email,
+                  let password = password
+            else { return }
+
+            Auth.auth().signIn(withEmail: email, password: password) { (authResult, error) in
+                guard authResult != nil, error == nil else {
+                    if let error = error {
+                        completion(.failure(error))
+                    }
+                    else {
+                        completion(.failure(AuthError.SignInFailed))
+                    }
+                    return
+                    
+                }
+                
+            }
+
+//        case .Facebook: completion(true)
+//
+//        case .Google: completion(true)
+        }
+    }
+    
+    
+    
+    public func SignUp(with username: String, email: String, password: String, completion: @escaping (Result<String, Error>) -> Void) {
+        // Make sure entered username is available 
         
+        Auth.auth().createUser(withEmail: email, password: password) { (result, error) in
+            guard result != nil, error == nil else {
+                if let error = error {
+                    print("From AuthManager")
+                    print(error)
+                    completion(.failure(error))
+                    return
+                }
+                else {
+                    completion(.failure(AuthError.SignUpFailed))
+                }
+                return
+            
+            }
+            DatabaseManager.shared.insertUser(with: email, username: username, completion: completion)
+            
+        }
     }
     
     
--- TikTok/Managers/DatabaseManager.swift
+++ TikTok/Managers/DatabaseManager.swift
@@ -6,3 +6,82 @@
 //
 
 import Foundation
+import FirebaseDatabase
+
+final class DatabaseManager {
+    public static let shared = DatabaseManager()
+    
+    private let database = Database.database().reference()
+    
+    private init() {}
+    
+    enum DatabaseError: Error {
+        case insertingError
+    }
+    
+    // Public
+    
+    public func insertUser(with email: String, username: String, completion: @escaping (Result<String, Error>) -> Void) {
+        /*
+         users: {
+         "roypark": {
+         email
+         posts: []
+         }
+         }
+         */
+        // users -> {}
+        // get current users key
+        // if that exists, insert new entry
+        // else create root users
+        
+        database.child("users").observeSingleEvent(of: .value) { [weak self] (snapshot) in
+            //            print(snapshot.value)
+            guard var usersDictionary = snapshot.value as? [String: Any] else {
+                // create users root node
+                self?.database.child("users").setValue(
+                    [
+                        username: [
+                            "email": email
+                        ]
+                    ]
+                ) { (error, _) in
+                    if let error = error {
+                        completion(.failure(error))
+                        return
+                    } else {
+                        completion(.success(email))
+                        return
+                    }
+                    
+                    
+                }
+                return
+            }
+            
+            usersDictionary[username] = ["email": email]
+            //            self?.database.child("users").setValue(usersDictionary, withCompletionBlock: { (error, _) in
+            //                guard error == nil else {
+            //                    completion(false)
+            //                    return
+            //                }
+            //
+            //                completion(true)
+            //
+            //            })
+            
+            self?.database.child("users").setValue(usersDictionary, withCompletionBlock: { (error, _) in
+                if let error = error {
+                    completion(.failure(error))
+                    return
+                }
+                completion(.success(email))
+                return
+            })
+        }
+    }
+    
+    public func getAllUsers(completion: ([String]) -> Void) {
+        
+    }
+}
