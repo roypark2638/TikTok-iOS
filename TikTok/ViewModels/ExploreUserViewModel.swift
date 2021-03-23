7c7d4d792cc7c7d290e8a92e0679d65b850b0b07
Roy Park <roypark2638@gmail.com>
2021-03-22 19:12:31 -0700

    Minor change

--- TikTok/ViewModels/ExploreUserViewModel.swift
+++ TikTok/ViewModels/ExploreUserViewModel.swift
@@ -10,7 +10,7 @@ import UIKit
 
 
 struct ExploreUserViewModel {
-    let profilePictureURL: URL
+    let profilePictureURL: URL?
     let username: String
     let followerCount: Int
     let handler: (() -> Void) // something happens, and we can tap on that cell
