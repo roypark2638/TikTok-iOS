ad0fadaff8073a80e902bfef5740a6270c2339d4
Roy Park <roypark2638@gmail.com>
2021-04-01 16:56:55 -0700

    Setup for when user tab a followers or following button to see the users

--- TikTok/Controllers/Other/UserListViewController.swift
+++ TikTok/Controllers/Other/UserListViewController.swift
@@ -7,23 +7,68 @@
 
 import UIKit
 
+// we are going to list your followers and following
+// responsible to show a list with a table view of respective things we are trying to look at
 class UserListViewController: UIViewController {
+    
+    private let tableView: UITableView = {
+        let tableView = UITableView(frame: .zero)
+        tableView.register(UITableViewCell.self,
+                           forCellReuseIdentifier: "cell")
+        
+        return tableView
+    }()
+    
+    enum ListType {
+        case followers
+        case following
+    }
+    
+    let type: ListType
+    let user: User
+    
+    init(type: ListType, user: User) {
+        self.type = type
+        self.user = user
+        super.init(nibName: nil, bundle: nil)
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError()
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.backgroundColor = .systemBackground
+        switch type {
+        case .followers: title = "Followers"
+        case .following: title = "Following"
+        }
 
-        // Do any additional setup after loading the view.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        tableView.frame = view.bounds
     }
-    */
+    
+}
 
+extension UserListViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
+        cell.backgroundColor = .systemPink
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, didDeselectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    }
 }
