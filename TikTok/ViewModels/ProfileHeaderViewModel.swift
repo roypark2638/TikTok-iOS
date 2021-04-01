19c268c6339b47d9fa3926569add4e5867e39054
Roy Park <roypark2638@gmail.com>
2021-04-01 16:57:26 -0700

    ProfileHeader View Model Setup

---
+++ TikTok/ViewModels/ProfileHeaderViewModel.swift
@@ -0,0 +1,15 @@
+//
+//  ProfileHeaderViewModel.swift
+//  TikTok
+//
+//  Created by Roy Park on 3/26/21.
+//
+
+import Foundation
+
+struct ProfileHeaderViewModel {
+    let profileImageURL: URL?
+    let followerCount: Int
+    let followingCount: Int
+    let isFollowing: Bool?
+}
