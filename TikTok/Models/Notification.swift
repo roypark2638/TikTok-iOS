ba55b6c71928b82a3b36bc83a1baecbc7aa62556
Roy Park <roypark2638@gmail.com>
2021-04-01 20:47:18 -0700

    Mock up data for 3 different notifications

--- TikTok/Models/Notification.swift
+++ TikTok/Models/Notification.swift
@@ -7,14 +7,61 @@
 
 import Foundation
 
-struct Notification {
+enum NotificationType{
+    case userFollow(username: String)
+    case postLike(postName: String)
+    case postComment(postName: String)
+    
+    var id: String {
+        switch self {
+        case .userFollow: return "userFollow"
+        case .postLike: return "postLike"
+        case .postComment: return "postComment"
+        }
+    }
+}
+
+class Notification {
+    var identifier = UUID().uuidString
+    var isHidden = false
     let text: String
+    let type: NotificationType
     let date: Date
     
+    init(text: String, type: NotificationType, date: Date) {
+        self.text = text
+        self.type = type
+        self.date = date
+    }
+    
     static func mockData() -> [Notification] {
-        return Array(0...100).compactMap({
-            Notification(text: "Notification: \($0)", date: Date())
+        
+        let first = Array(0...3).compactMap({
+            Notification(
+                text: "Notification: \($0)",
+                type: .postLike(postName: "Good Post"),
+                date: Date()
+            )
         })
+        
+        let second = Array(0...3).compactMap({
+            Notification(
+                text: "Notification: \($0)",
+                type: .postComment(postName: "How are you"),
+                date: Date()
+            )
+        })
+        
+        let third = Array(0...3).compactMap({
+            Notification(
+                text: "Notification: \($0)",
+                type: .userFollow(username: "Roy Park"),
+                date: Date()
+            )
+        })
+        
+        return first + second + third
+        
     }
     
 }
