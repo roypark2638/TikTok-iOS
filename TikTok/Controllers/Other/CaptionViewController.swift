45de9e96c107daa496345b32f43fa1a6014f63fe
Roy Park <roypark2638@gmail.com>
2021-04-03 14:13:41 -0700

    Setup CaptionVC

---
+++ TikTok/Controllers/Other/CaptionViewController.swift
@@ -0,0 +1,39 @@
+//
+//  CaptionViewController.swift
+//  TikTok
+//
+//  Created by Roy Park on 4/3/21.
+//
+
+import UIKit
+
+class CaptionViewController: UIViewController {
+    
+    let videoURL: URL
+    
+    init(videoURL: URL) {
+        self.videoURL = videoURL
+        super.init(nibName: nil, bundle: nil)
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError()
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "Add Caption"
+        view.backgroundColor = .systemBackground
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Post", style: .done, target: self, action: #selector(didTapPost))
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+    }
+        
+    @objc private func didTapPost() {
+        
+    }
+    
+
+}
